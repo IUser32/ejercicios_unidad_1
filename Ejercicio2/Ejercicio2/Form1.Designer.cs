6d746b6c7d1b0a8dd9acc20252d6a36c6376bdb1
Luis Fortuna <javier-uribe@hotmail.com>
2025-09-18 20:25:11 -0400

    test

--- Ejercicio2/Ejercicio2/Form1.Designer.cs
+++ Ejercicio2/Ejercicio2/Form1.Designer.cs
@@ -29,6 +29,10 @@
         private void InitializeComponent()
         {
             label1 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            label2 = new Label();
             SuspendLayout();
             // 
             // label1
@@ -43,11 +47,52 @@
             label1.TabIndex = 0;
             label1.Text = "Yo soy LUIS FORTUNA, Matricula LR-2025-005425";
             // 
+            // button1
+            // 
+            button1.Location = new Point(1169, 232);
+            button1.Name = "button1";
+            button1.Size = new Size(188, 58);
+            button1.TabIndex = 1;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(669, 183);
+            button2.Name = "button2";
+            button2.Size = new Size(188, 58);
+            button2.TabIndex = 2;
+            button2.Text = "Guardar";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(1060, 399);
+            button3.Name = "button3";
+            button3.Size = new Size(188, 58);
+            button3.TabIndex = 3;
+            button3.Text = "button3";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(628, 373);
+            label2.Name = "label2";
+            label2.Size = new Size(97, 41);
+            label2.TabIndex = 4;
+            label2.Text = "label2";
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(17F, 41F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1472, 214);
+            ClientSize = new Size(1413, 635);
+            Controls.Add(label2);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
             Controls.Add(label1);
             Name = "Form1";
             Text = "Form1";
@@ -58,5 +103,9 @@
         #endregion
 
         private Label label1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Label label2;
     }
 }
--- Ejercicio2/Ejercicio2/Form1.cs
+++ Ejercicio2/Ejercicio2/Form1.cs
@@ -6,5 +6,10 @@ namespace Ejercicio2
         {
             InitializeComponent();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Esto es un mensaje");
+        }
     }
 }
